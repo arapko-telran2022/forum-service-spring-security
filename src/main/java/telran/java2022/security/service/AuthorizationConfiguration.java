74720df2f0a76ac0c4b2d14760fa23b65168eb9a
arapko <1990rapko@gmail.com>
2022-11-16 21:46:41 +0200

    Endpoints filters commit

--- src/main/java/telran/java2022/security/service/AuthorizationConfiguration.java
+++ src/main/java/telran/java2022/security/service/AuthorizationConfiguration.java
@@ -1,17 +1,23 @@
 package telran.java2022.security.service;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.Authentication;
+
+import lombok.RequiredArgsConstructor;
+import telran.java2022.post.dao.PostRepository;
+import telran.java2022.post.model.Post;
 
 @EnableWebSecurity
 public class AuthorizationConfiguration extends WebSecurityConfigurerAdapter {
 	
+	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		
 		
 		http.httpBasic();
 		http.csrf().disable();
@@ -23,12 +29,31 @@ public class AuthorizationConfiguration extends WebSecurityConfigurerAdapter {
 		.antMatchers("/forum/posts/**").permitAll();
 		
 		http.authorizeRequests()
-		.regexMatchers(HttpMethod.DELETE, "/account/user/\\w+/role/\\w+/?").access("hasRole('ADMINISTRATOR')")
-		.regexMatchers(HttpMethod.PUT, "/account/user/\\w+/role/\\w+/?").hasRole("ADMINISTRATOR")
-		.regexMatchers(HttpMethod.DELETE, "/forum/post/\\w+/?").hasAnyRole("MODERATAOR", "ADMINISTRATOR");
-		
+//		-----------------------------------------------------------------------------------------------------------		
+		.antMatchers(HttpMethod.DELETE, "/account/user/{user}/role/{role}/**")
+			.access("hasRole('ADMINISTRATOR')")
+		.antMatchers(HttpMethod.PUT, "/account/user/{user}/role/{role}/**")
+			.access("hasRole('ADMINISTRATOR')")	
+		.antMatchers(HttpMethod.DELETE, "/account/user/{login}/**")
+			.access("hasRole('ADMINISTRATOR') or @ownerChecker.checkUserOwner(authentication,#login)")	
+		.antMatchers(HttpMethod.PUT, "/account/user/{login}/**")
+			.access("@ownerChecker.checkUserOwner(authentication,#login)")
+		.antMatchers(HttpMethod.PUT, "/account/password/**")
+			.access("@ownerChecker.checkUserOwner(authentication,#login)")			
+//		-----------------------------------------------------------------------------------------------------------
+		.antMatchers(HttpMethod.POST, "/forum/post/{author}/**")
+			.access("@ownerChecker.checkUserOwner(authentication,#author)")			
+		.antMatchers(HttpMethod.DELETE, "/forum/post/{id}/**")
+			.access("hasRole('MODERATAOR') or @ownerChecker.checkPostOwner(authentication,#id)")
+		.antMatchers(HttpMethod.PUT, "/forum/post/{id}/**")
+			.access("hasRole('MODERATAOR') or @ownerChecker.checkPostOwner(authentication,#id)")
+		.antMatchers(HttpMethod.PUT, "/forum/post/{id}/comment/{author}/**")
+			.access("@ownerChecker.checkUserOwner(authentication,#author)");
+//		-----------------------------------------------------------------------------------------------------------		
 		http.authorizeRequests().anyRequest().authenticated();
 		
 	} 
 	
+	
 }
+
---
+++ src/main/java/telran/java2022/security/service/OwnerChecker.java
@@ -0,0 +1,27 @@
+package telran.java2022.security.service;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+import lombok.RequiredArgsConstructor;
+import telran.java2022.post.dao.PostRepository;
+import telran.java2022.post.model.Post;
+
+@Component
+@RequiredArgsConstructor
+public class OwnerChecker {
+	
+    private final PostRepository postRepository;;
+
+    public Boolean checkUserOwner(Authentication authentication, String id) {
+		Post post = postRepository.findById(id).get();
+		if (post == null) {
+			return false;
+		}
+		return authentication.getName().equals(post.getAuthor());
+	}
+
+	public Boolean checkPostOwner(Authentication authentication, String user) {
+	    return authentication.getName().equals(user);
+	}
+}
