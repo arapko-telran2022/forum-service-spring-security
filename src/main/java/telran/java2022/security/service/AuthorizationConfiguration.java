c113560c8d8c3fc438c249281cb10cbafce1d4af
arapko <1990rapko@gmail.com>
2022-11-16 15:32:35 +0200

    Init commit

---
+++ src/main/java/telran/java2022/security/service/AuthorizationConfiguration.java
@@ -0,0 +1,34 @@
+package telran.java2022.security.service;
+
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@EnableWebSecurity
+public class AuthorizationConfiguration extends WebSecurityConfigurerAdapter {
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		
+		
+		http.httpBasic();
+		http.csrf().disable();
+		
+		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+		
+		http.authorizeRequests()
+		.antMatchers("/account/register/**").permitAll()
+		.antMatchers("/forum/posts/**").permitAll();
+		
+		http.authorizeRequests()
+		.regexMatchers(HttpMethod.DELETE, "/account/user/\\w+/role/\\w+/?").access("hasRole('ADMINISTRATOR')")
+		.regexMatchers(HttpMethod.PUT, "/account/user/\\w+/role/\\w+/?").hasAuthority("ADMINISTRATOR")
+		.regexMatchers(HttpMethod.DELETE, "/forum/post/\\w+/?").hasAnyAuthority("MODERATAOR", "ADMINISTRATOR");
+		
+		http.authorizeRequests().anyRequest().authenticated();
+		
+	} 
+	
+}
