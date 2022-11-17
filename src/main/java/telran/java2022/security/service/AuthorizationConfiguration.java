961b32b82f6e40555cba5303d07bc7b2fc570245
arapko <1990rapko@gmail.com>
2022-11-17 08:48:43 +0200

    Bug ROLE name fixing commit

--- src/main/java/telran/java2022/security/service/AuthorizationConfiguration.java
+++ src/main/java/telran/java2022/security/service/AuthorizationConfiguration.java
@@ -1,16 +1,10 @@
 package telran.java2022.security.service;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.Authentication;
-
-import lombok.RequiredArgsConstructor;
-import telran.java2022.post.dao.PostRepository;
-import telran.java2022.post.model.Post;
 
 @EnableWebSecurity
 public class AuthorizationConfiguration extends WebSecurityConfigurerAdapter {
@@ -29,7 +23,7 @@ public class AuthorizationConfiguration extends WebSecurityConfigurerAdapter {
 		.antMatchers("/forum/posts/**").permitAll();
 		
 		http.authorizeRequests()
-//		-----------------------------------------------------------------------------------------------------------		
+		
 		.antMatchers(HttpMethod.DELETE, "/account/user/{user}/role/{role}/**")
 			.access("hasRole('ADMINISTRATOR')")
 		.antMatchers(HttpMethod.PUT, "/account/user/{user}/role/{role}/**")
@@ -40,16 +34,16 @@ public class AuthorizationConfiguration extends WebSecurityConfigurerAdapter {
 			.access("@ownerChecker.checkUserOwner(authentication,#login)")
 		.antMatchers(HttpMethod.PUT, "/account/password/**")
 			.access("@ownerChecker.checkUserOwner(authentication,#login)")			
-//		-----------------------------------------------------------------------------------------------------------
+			
 		.antMatchers(HttpMethod.POST, "/forum/post/{author}/**")
 			.access("@ownerChecker.checkUserOwner(authentication,#author)")			
 		.antMatchers(HttpMethod.DELETE, "/forum/post/{id}/**")
-			.access("hasRole('MODERATAOR') or @ownerChecker.checkPostOwner(authentication,#id)")
+			.access("hasRole('MODERATOR') or @ownerChecker.checkPostOwner(authentication,#id)")
 		.antMatchers(HttpMethod.PUT, "/forum/post/{id}/**")
-			.access("hasRole('MODERATAOR') or @ownerChecker.checkPostOwner(authentication,#id)")
+			.access("hasRole('MODERATOR') or @ownerChecker.checkPostOwner(authentication,#id)")
 		.antMatchers(HttpMethod.PUT, "/forum/post/{id}/comment/{author}/**")
 			.access("@ownerChecker.checkUserOwner(authentication,#author)");
-//		-----------------------------------------------------------------------------------------------------------		
+
 		http.authorizeRequests().anyRequest().authenticated();
 		
 	} 
