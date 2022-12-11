0f74971112cfda122a72c87fce0adfa591b2c1df
arapko <1990rapko@gmail.com>
2022-12-11 13:53:50 +0200

    Fix commit

--- src/test/java/telran/java2022/ForumServiceSpringSecurityApplicationTests.java
+++ src/test/java/telran/java2022/ForumServiceSpringSecurityApplicationTests.java
@@ -6,7 +6,6 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 
